ce2c8c01c9b7f884114d8831e413a88a2b2ec975
Zeynep Sena Dogan <zeynepsena2002.d@gmail.com>
2023-10-20 20:48:26 +0300

    add login page

---
+++ lib/ui/auth/login_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../widgets/bottom_nav_bar.dart';
+
+class LoginPage extends ConsumerWidget {
+  LoginPage({super.key});
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+        key: scaffoldKey,
+        bottomNavigationBar: const NavBar(),
+        body: Stack(
+          children: <Widget>[
+            const Positioned.fill(
+              child: Image(
+                image: AssetImage('assets/img/login_bg.png'),
+                fit: BoxFit.cover,
+                //opacity: AlwaysStoppedAnimation(0.7),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: size.height * 0.1,
+                left: size.width * 0.03,
+                right: size.width * 0.03,
+              ),
+              child: const AuthSocialWidget(),
+            ),
+          ],
+        ));
+  }
+}
