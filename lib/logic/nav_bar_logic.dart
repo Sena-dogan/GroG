eb69b6f31fca3099e795da257b6d3da17d4c0f4a
Zeynep Sena Dogan <zeynepsena2002.d@gmail.com>
2023-10-20 20:46:07 +0300

    bottom nav bar logic

---
+++ lib/logic/nav_bar_logic.dart
@@ -0,0 +1,19 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../models/nav_bar_ui_model.dart';
+
+
+
+part 'nav_bar_logic.g.dart';
+
+@riverpod
+class BottomNavBarLogic extends _$BottomNavBarLogic {
+  @override
+  BottomNavBarUiModel build() {
+    return const BottomNavBarUiModel();
+  }
+
+  void setNavIndex(int index) {
+    state = state.copyWith(navIndex: index);
+  }
+}
