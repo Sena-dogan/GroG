fe0127e7322e9895970ad70875db321555e0cd68
Zeynep Sena Dogan <zeynepsena2002.d@gmail.com>
2023-10-20 19:04:07 +0300

    package updates

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,7 @@ import Foundation
 
 import connectivity_plus
 import firebase_core
-import path_provider_macos
+import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
 
