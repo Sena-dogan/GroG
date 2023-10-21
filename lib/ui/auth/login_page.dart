48225cadd326fcce8c32c006ab2599becbf5167f
Zeynep Sena Dogan <zeynepsena2002.d@gmail.com>
2023-10-21 14:01:55 +0300

    auth widget

--- lib/ui/auth/login_page.dart
+++ lib/ui/auth/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../widgets/bottom_nav_bar.dart';
+import 'auth_widget.dart';
 
 class LoginPage extends ConsumerWidget {
   LoginPage({super.key});
