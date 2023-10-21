fb17d755012744e22fa79bae30d11d3ba3db27a6
Zeynep Sena Dogan <zeynepsena2002.d@gmail.com>
2023-10-21 14:01:39 +0300

    login logic

---
+++ lib/ui/auth/login_logic.dart
@@ -0,0 +1,78 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+
+import '../../models/login_ui_model.dart';
+
+part 'login_logic.g.dart';
+
+@riverpod
+class LoginLogic extends _$LoginLogic {
+  @override
+  LoginUiModel build() {
+    return const LoginUiModel();
+  }
+
+  void setLogin({bool isLoading = false}) {
+    state = state.copyWith(isLoading: isLoading);
+  }
+
+  void clear() {
+    state = state.copyWith(error: null, isLoading: false);
+  }
+
+  Future<void> signInWithGoogle() async {
+    try {
+      setLogin(isLoading: true);
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser!.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      await FirebaseAuth.instance.signInWithCredential(credential);
+      setLogin();
+    } catch (e) {
+      debugPrint(e.toString());
+      setError(e.toString());
+      setLogin();
+    }
+  }
+
+  Future<void> signInWithApple() async {
+    debugPrint('hello my name is apple. i am 7 years old. i like to eat apples');
+    try {
+    debugPrint('AAAAAAAAAA my name is apple. i am 7 years old. i like to eat apples');
+      setLogin(isLoading: true);
+      final AuthorizationCredentialAppleID appleCredential =
+          await SignInWithApple.getAppleIDCredential(
+        scopes: <AppleIDAuthorizationScopes>[
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+      );
+      final OAuthProvider oAuthProvider = OAuthProvider('apple.com');
+      final AuthCredential credential = oAuthProvider.credential(
+        idToken: appleCredential.identityToken,
+        accessToken: appleCredential.authorizationCode,
+      );
+      await FirebaseAuth.instance.signInWithCredential(credential);
+      setLogin();
+    } catch (e) {
+      debugPrint(e.toString());
+      setError(e.toString());
+      setLogin();
+    }
+  }
+
+  void setError(String error) {
+    state = state.copyWith(error: error);
+  }
+
+  void setLoggedIn(bool isLoggedIn) {
+    state = state.copyWith(isLoggedIn: isLoggedIn);
+  }
+}
---
+++ lib/ui/auth/login_logic.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_logic.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$loginLogicHash() => r'd4fcd5bac6233c96c80329b6d58c3416499c2de1';
+
+/// See also [LoginLogic].
+@ProviderFor(LoginLogic)
+final loginLogicProvider =
+    AutoDisposeNotifierProvider<LoginLogic, LoginUiModel>.internal(
+  LoginLogic.new,
+  name: r'loginLogicProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$loginLogicHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$LoginLogic = AutoDisposeNotifier<LoginUiModel>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
