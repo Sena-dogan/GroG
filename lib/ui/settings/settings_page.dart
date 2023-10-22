4f6de7ff0d1ac02f032febec7ef39f521fd85fd9
Zeynep Sena Dogan <zeynepsena2002.d@gmail.com>
2023-10-22 21:46:32 +0300

    settings page

---
+++ lib/ui/settings/settings_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
+
+import '../utils/context_extensions.dart';
+import '../utils/gradient_bg.dart';
+import '../widgets/bottom_nav_bar.dart';
+import '../widgets/theme_widget.dart';
+
+class SettingsPage extends ConsumerWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      bottomNavigationBar: const NavBar(),
+      body: GradientBackground(
+        child: Column(
+          children: <Widget>[
+            Gap(size.height * 0.1),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: ThemeWidget(),
+            ),
+            const Gap(30),
+            SettingsCard(
+              title: 'Login',
+              onTap: () => context.go('/login'),
+            ),
+            SettingsCard(
+              title: 'Logout',
+              onTap: () => context.go('/logout'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsCard extends StatelessWidget {
+  const SettingsCard({
+    super.key,
+    required this.title,
+    required this.onTap,
+  });
+
+  final String title;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: context.theme.primaryColorDark,
+        ),
+        child: ListTile(
+          title: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Text(title, style: context.theme.primaryTextTheme.bodyLarge),
+          ),
+          onTap: onTap,
+        ),
+      ),
+    );
+  }
+}
