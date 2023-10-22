474ab5913b6067c7942f7fd9e8c0f145a552a231
Zeynep Sena Dogan <zeynepsena2002.d@gmail.com>
2023-10-22 21:44:37 +0300

    add grog colors

---
+++ lib/ui/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class GrogColors {
+  static const Color ScaffoldBackGroundColor = Color(0xFF050405);
+  static const Color HomeLinearColor1 = Color(0xFF17101C);
+  static const Color HomeLinearColor2 = Color(0xFF110E18);
+}
