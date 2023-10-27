997fd14fd560cc555771ac2e6ee2c5004e6a9bb5
Zeynep Sena Dogan <zeynepsena2002.d@gmail.com>
2023-10-27 14:39:11 +0300

    assets

--- lib/ui/utils/assets.dart
+++ lib/ui/utils/assets.dart
@@ -5,5 +5,11 @@ class Assets {
   static const String appLogoSmall = 'assets/images/AppLogoSmall.svg';
   static const String appCompany = 'assets/images/company.svg';
   static const String loginImage = 'assets/images/login_image.png';
-  static const String homeImage = 'assets/images/homeimage.png';
+  static const String homePostImage = 'assets/images/homeimage.png';
+  static const String homeCatCocktails = 'assets/images/HomeCatCocktails.png';
+  static const String homeCatSurpriseMe = 'assets/images/HomeCatSurpriseMe.png';
+  static const String homeCatMocktail = 'assets/images/HomeCatMocktail.png';
+  static const String homeIconCocktail = 'assets/images/icon_cocktail.png';
+  static const String homeIconMocktail = 'assets/images/icon_mocktail.png';
+  static const String homeIconSurpriseMe = 'assets/images/icon_surprise_me.png';
 }
